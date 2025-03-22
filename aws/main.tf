5b52c6e962d20b09fc82dd8c6db286470727dc44
kbKenz <147579910+kbKenz@users.noreply.github.com>
2025-03-22 11:02:14 +0600

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,27 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "lead-distribution-app-bucket"
 }
+resource "aws_db_instance" "postgres" {
+  identifier        = "lead-distribution-postgres"
+  engine            = "postgres"
+  engine_version    = "15.3"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  storage_type      = "gp2"
+
+  db_name  = "odoo"
+  username = "odoo"
+  password = "odoo"
+
+  skip_final_snapshot = true
+  publicly_accessible = false
+
+  # Free tier settings
+  multi_az = false
+
+  # Use cheapest storage and IOPS
+  iops = 0
+
+  # Basic parameter group
+  parameter_group_name = "default.postgres15"
+}
